235ff5360cb6eae752e587c8c5c896087be76b9b
Rohan Nayan <rohannayan405@gmail.com>
2023-05-22 23:56:25 +0530

    Added React Props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import Note from "./Note";
 import Header from "./Header";
 import Footer from "./Footer";
+import notes from '../notes';
+
+function createNote(notes){
+    return (
+        <Note 
+            key = {notes.key}
+            title = {notes.title}
+            content = {notes.content}
+        />
+    );
+}
 
 function App(){
     return(
         <div>
             <Header />
-            <Note />
+            {notes.map(createNote)}
             <Footer />
         </div>
     );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
-const title = "Note Title";
-const content = "Note Content";
-
-function Note(){
+function Note(props){
     return(
         <div className='note'>
-            <h1>{title}</h1>
-            <p>{content}</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     )
 }
