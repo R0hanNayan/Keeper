91c0fa459d81919e947008506853aefbf408d4cb
Rohan Nayan <rohannayan405@gmail.com>
2023-06-11 17:03:15 +0530

    Added Add and Delete Functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import Note from "./Note";
 import Header from "./Header";
 import Footer from "./Footer";
-import notes from '../notes';
 import CreateArea from "./CreateArea";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function addNote(newNote){
+        setNotes(prevNote=>{
+           return [...prevNote, newNote];
+        });
+    }
+    function deleteNote(id){
+        setNotes(prevNotes=>{
+            return(prevNotes.filter((noteItem, index)=>{
+                return(index!==id);
+            }));
+        });
+    }
+
     return (
         <div>
             <Header />
-            {notes.map(noteItems => {
-                return (
-                    <Note
-                        key={1} 
-                        title="Note title" 
-                        content="Note content"
-                    />);
+            <CreateArea 
+                onAdd={addNote}
+            />
+            {notes.map((noteItem, index)=>{
+                return(
+                <Note
+                    key={index}
+                    id={index}
+                    title={noteItem.title}
+                    content={noteItem.content}
+                    onDelete={deleteNote}
+                />)
             })}
             < Footer />
         </div>
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
-  return (
-    <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+function CreateArea(props) {
+    const [note, setnote] = useState({
+        title: "",
+        content: ""
+    })
+
+    function handleChange(event){
+        const {name, value} = event.target;
+
+        setnote(prevNote =>{
+            return {
+                ...prevNote,
+                [name]: value
+            };
+        });
+    }
+
+    function submitNote(event){
+        props.onAdd(note);
+        setnote({
+            title: "",
+            content: ""
+          });
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <form>
+                <input onChange={handleChange} name="title" value={note.title} placeholder="Title" />
+                <textarea onChange={handleChange} name="content" value={note.content} placeholder="Take a note..." rows="3" />
+                <button onClick={submitNote}>Add</button>
+            </form>
+        </div>
+    );
 }
 
 export default CreateArea;
