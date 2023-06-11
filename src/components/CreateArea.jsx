2f9237b7c58d7fef6f00d49c18e8050bc1b39262
Rohan Nayan <rohannayan405@gmail.com>
2023-06-11 21:07:16 +0530

    Added Material UI and Icons

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,40 +1,64 @@
 import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function CreateArea(props) {
-    const [note, setnote] = useState({
-        title: "",
-        content: ""
-    })
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
 
-    function handleChange(event){
-        const {name, value} = event.target;
+  const [noteState, setNoteState] = useState(false);
+  function expand() {
+    setNoteState(true);
+  }
 
-        setnote(prevNote =>{
-            return {
-                ...prevNote,
-                [name]: value
-            };
-        });
-    }
+  function handleChange(event) {
+    const { name, value } = event.target;
 
-    function submitNote(event){
-        props.onAdd(note);
-        setnote({
-            title: "",
-            content: ""
-          });
-        event.preventDefault();
-    }
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
 
-    return (
-        <div>
-            <form>
-                <input onChange={handleChange} name="title" value={note.title} placeholder="Title" />
-                <textarea onChange={handleChange} name="content" value={note.content} placeholder="Take a note..." rows="3" />
-                <button onClick={submitNote}>Add</button>
-            </form>
-        </div>
-    );
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        {noteState ? <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        /> : null}
+        <textarea
+          name="content"
+          onClick={expand}
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows={noteState ? 3 : 1}
+        />
+        <Zoom in={noteState}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
+      </form>
+    </div >
+  );
 }
 
 export default CreateArea;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import EditNoteIcon from '@mui/icons-material/EditNote';
 
 function Header(){
     return (
         <header>
-            <h1>Keeper</h1>
+            <h1><EditNoteIcon />Keeper</h1>
         </header>
     );
 }
